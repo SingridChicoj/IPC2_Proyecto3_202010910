548ef3c6209bae0e09a51f732fc4074e5f33e390
Singrid Martinez <3403252160101@ingenieria.usac.edu.gt>
2023-10-25 23:54:57 -0600

    Commit

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({
+            "message": "No se ha enviado un archivo"
+        })
+    archivo = request.files['file']
+    contenidoarchivo = archivo.read().decode('utf-8')
+    print(contenidoarchivo)
+    return jsonify({
+        "message": "Archivo cargado"
+    })
 
+@app.route('/flask_response', methods=['GET'])
+def get_response_from_flask():
+    response_data={
+        "message": "Respuesta desde flask al pedido GET"
+    }
+    return jsonify(response_data)
+
+@app.route('/flask_response2', methods=['GET'])
+def get_response_from_flask2():
+    response_data={
+        "message": "Otra respuesta desde flask al pedido GET"
+    }
+    return jsonify(response_data)
 
 if __name__ == "__main__":
-    app.run(threaded=True, port=3000, debug=True)
+    app.run(threaded=True, port=5000, debug=True)
---
+++ partedjango/myapp/views.py
@@ -0,0 +1,44 @@
+import requests
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
+def myform_view(request):
+    if request.method == 'POST':
+        data = request.POST.get("data")
+        file = request.FILES.get("file")
+
+        if not file:
+            return JsonResponse({
+                "message": "No se ha seleccionado un archivo"
+            })
+        
+        try:
+            files = {"file": (file.name, file.read())}
+            response = requests.post('http://127.0.0.1:5000/upload_file', data={"data": data}, files=files)
+            response.raise_for_status()
+
+            #Procesa la respuesta del backend
+            response_data = response.json()
+            return JsonResponse(response_data)
+        
+        except requests.exceptions.RequestException as e:
+            return HttpResponse(str(e), status=500)
+    return render(request, 'myform.html')
+
+def get_response_from_flask(request):
+    try:
+        response = requests.get('http://127.0.0.1:5000/flask_response')
+        response.raise_for_status()
+        response_data = response.json()
+        return JsonResponse(response_data)
+    except requests.exceptions.RequestException as e:
+            return HttpResponse(str(e), status=500)
+
+def get_response_from_flask2(request):
+    try:
+        response = requests.get('http://127.0.0.1:5000/flask_response2')
+        response.raise_for_status()
+        response_data = response.json()
+        return JsonResponse(response_data)
+    except requests.exceptions.RequestException as e:
+            return HttpResponse(str(e), status=500)
